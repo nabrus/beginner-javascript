120e7c54e41b7f8c65ba66ce1b271143a331793b
towelie <34988577+nabrus@users.noreply.github.com>
2020-01-26 15:12:02 -0500

    update types for nums

--- playground/types.js
+++ playground/types.js
@@ -1,15 +1,16 @@
 /* eslint-disable */
-const name = 'Steve'; // single quotes
-const middle = "John"; // double quotes
-const last = `Urban`; // back-tics
+// const name = 'Steve'; // single quotes
+// const middle = "John"; // double quotes
+// const last = `Urban`; // back-tics
 
-const hello = `Hello my name is ${name}. Nice to meet you. I am ${50 + 200} years old`;
+// const hello = `Hello my name is ${name}. Nice to meet you. I am ${50 + 200} years old`;
 
-const html = `
-  <div>
-    <h2>${name}<h2>
-    <p>${hello}<p>
-  </div>
-`;
+// const html = `
+//   <div>
+//     <h2>${name}<h2>
+//     <p>${hello}<p>
+//   </div>
+// `;
 
-console.log(html);
+// console.log(html);
+
