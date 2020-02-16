fec9947233c994a850f377253c3ec6e6a0c7a053
towelie <34988577+nabrus@users.noreply.github.com>
2020-02-16 14:53:04 -0500

    complete numbers section

--- playground/types.js
+++ playground/types.js
@@ -14,3 +14,24 @@
 
 // console.log(html);
 
+
+/* MATH */
+
+const smarties = 20;
+const kids = 3;
+const eachKidGets = Math.floor(smarties / kids);
+
+console.log(`Each kid gets ${eachKidGets} smarties`);
+
+console.log(`And dad gets ${smarties % kids}`);
+
+// When working with money always store values in cents
+// due to Floating Point Math.
+
+// Instead of price = 10.34;
+const price = 1034;
+
+// Infinity and -Infinity are a "number", expressing a number higher than the 
+// comp can calculate.
+
+// NaN (not a number) = "number"
