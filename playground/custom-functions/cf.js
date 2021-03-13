58beb0f4e23bc86678cc9cebd11341ee67931f72
towelie <34988577+nabrus@users.noreply.github.com>
2021-03-13 10:35:14 -0500

    Add params and args section

--- playground/custom-functions/cf.js
+++ playground/custom-functions/cf.js
@@ -1,7 +1,20 @@
+/* Functions */
+
+// Basic Structure
 // Defining a Function or Function Definition
 function calculateBill() {
   // Function body or block
 }
 
-// Function call or **Run**, running a function
+// Call, Run or Invoke a function
 calculateBill();
+
+// Parameters and Arguments
+
+function calMyBill(billAmt, taxRate) {
+  const total = billAmt * 1 + taxRate;
+  return total;
+}
+
+const myTotal = calMyBill(200, 0.13);
+console.log(calMyBill(100, 0.13), myTotal);
