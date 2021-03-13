8e5573b82c1e49d819fc3927f3a5e46d1288479c
towelie <34988577+nabrus@users.noreply.github.com>
2021-03-13 15:46:23 -0500

    Add passing vars as args and passing expressions examples

--- playground/custom-functions/cf.js
+++ playground/custom-functions/cf.js
@@ -1,6 +1,6 @@
-/* Functions */
+/* FUNCTIONS */
 
-// Basic Structure
+// Basic  Structure
 // Defining a Function or Function Definition
 function calculateBill() {
   // Function body or block
@@ -9,7 +9,7 @@ function calculateBill() {
 // Call, Run or Invoke a function
 calculateBill();
 
-// Parameters and Arguments //
+// PARAMETERS and ARGUMENTS //
 
 // Parameters are the placeholders in a function
 // Arguments are the values passed in when calling a function
@@ -22,8 +22,21 @@ function calMyBill(billAmt, taxRate) {
 const myTotal = calMyBill(200, 0.13);
 console.log(calMyBill(100, 0.13), myTotal); // 100.13 200.13
 
-// Passing variables as args
+// Passing variables as args //
 const theDudesTotal = 500;
 const theDudesTax = 0.3;
 const lebowskiTotal = calMyBill(theDudesTotal, theDudesTax);
 console.log(lebowskiTotal); // 500.3
+
+// Another example of defining a function
+// then setting the invoked function with an argument in a variable
+function sayHiTo(firstName) {
+  return `Hi ${firstName}!`;
+}
+
+const greeting = sayHiTo('Luke');
+console.log(greeting); // Hi Luke!
+
+// Passing expressions //
+const myTotal2 = calMyBill(20 + 20 + 30 + 20, 0.15);
+console.log(myTotal2); // 90.15
