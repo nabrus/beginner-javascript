d7dc667fdfa90845d97b5b04fbf318033dedae1a
towelie <34988577+nabrus@users.noreply.github.com>
2021-03-13 15:31:56 -0500

    Add 1st example of params and args

--- playground/custom-functions/cf.js
+++ playground/custom-functions/cf.js
@@ -9,7 +9,10 @@ function calculateBill() {
 // Call, Run or Invoke a function
 calculateBill();
 
-// Parameters and Arguments
+// Parameters and Arguments //
+
+// Parameters are the placeholders in a function
+// Arguments are the values passed in when calling a function
 
 function calMyBill(billAmt, taxRate) {
   const total = billAmt * 1 + taxRate;
@@ -17,4 +20,10 @@ function calMyBill(billAmt, taxRate) {
 }
 
 const myTotal = calMyBill(200, 0.13);
-console.log(calMyBill(100, 0.13), myTotal);
+console.log(calMyBill(100, 0.13), myTotal); // 100.13 200.13
+
+// Passing variables as args
+const theDudesTotal = 500;
+const theDudesTax = 0.3;
+const lebowskiTotal = calMyBill(theDudesTotal, theDudesTax);
+console.log(lebowskiTotal); // 500.3
