2b9b693445b0f818b569422c6db2f67773a00f18
butters <34988577+nabrus@users.noreply.github.com>
2021-03-16 22:46:32 -0400

    Complete arrow functions

--- playground/custom-functions/ways-to-make-a-function.js
+++ playground/custom-functions/ways-to-make-a-function.js
@@ -13,4 +13,29 @@ const doctorize2 = function(firstName) {
   return `Dr. ${firstName}`;
 };
 
-// Arrow Functions
+// Arrow Functions //
+// Function declaration
+function inchToCM(inches) {
+  const cm = inches * 2.54;
+  return cm;
+  // Can also condense to `return inches * 2.54`
+}
+
+console.log(inchToCM(3));
+
+// Converted to an arrow function
+const inchToCMArrow = inches => inches * 2.54;
+
+console.log(inchToCMArrow(6));
+
+// Another example
+function add(a, b = 3) {
+  return a + b;
+}
+
+console.log(add(4)); // 7
+
+// Converted to an arrow function
+const addArrow = (a, b = 3) => a + b;
+
+console.log(addArrow(7)); // 10
