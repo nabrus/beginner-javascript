641141c1ff845e8b58aaf57fa20dc7e773cba422
towelie <34988577+nabrus@users.noreply.github.com>
2021-03-20 10:04:28 -0400

    Complete ways to make functions

--- playground/custom-functions/ways-to-make-a-function.js
+++ playground/custom-functions/ways-to-make-a-function.js
@@ -1,15 +1,15 @@
-// Function Definition, Function Declaration, or Function Statement
+// FUNCTION DECLARATION  (Function Statement, or Function Definition) //
 function doctorize(firstName) {
   return `Dr. ${firstName}`;
 }
 
-// Function expressions - storing a function as a value in a variable
+// FUNCTION EXPRESSIONS - storing a function as a value in a variable //
 // This also is an anonymous function - a function with no name
 const doctorize2 = function(firstName) {
   return `Dr. ${firstName}`;
 };
 
-// Arrow Functions //
+// ARROW FUNCTION //
 // Function declaration
 function inchToCM(inches) {
   const cm = inches * 2.54;
@@ -43,7 +43,7 @@ console.log(addArrow(7)); // 10
 })();
 
 /* eslint-disable */
-// Method - a function which is a property of an object. Or simply, functions that live inside objects.
+// METHOD - a function which is a property of an object. Or simply, functions that live inside objects. //
 
 const threepio = {
   name: 'C3PO',
@@ -67,4 +67,26 @@ const threepio = {
 threepio.sayHi(); // is a method
 threepio.complain(); // is a method
 
-// Callback function - a function passed into another function as an argument, which is then invoked inside the outer function to complete some action.
+// CALLBACK FUNCTION - a function passed into another function as an argument, which is then invoked inside the outer function to complete some action. //
+// Click callback example
+const buttonA = document.querySelector('.clickMe');
+const buttonB = document.querySelector('.dontClick');
+const buttonC = document.querySelector('.comeOnNow');
+
+function handleClick() {
+  console.log('Stop clicking ME!!!!');
+}
+
+// Passing in callback functions when the function was declared outside
+buttonB.addEventListener('click', handleClick); // handleClick is the callback function
+buttonA.addEventListener('click', threepio.sayHi); // threepio.sayHi is the callback
+
+// Passing an anonymous function inside directly (arrow function syntax)
+buttonC.addEventListener('click', () => console.log('Enough with the clicking!'));
+
+// Another example would be a timer callback using `setTimeout`
+setTimeout(threepio.scold, 1000); // after a second `threepio.scold` will run
+// or pass in an anonymous function
+setTimeout(function() {
+  console.log('Beep Beep Bloop Beep');
+}, 2000); // after 2 seconds the anonymous function will run
