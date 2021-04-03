3c87abcfb67e7339b2dba2256e5240e1e477e4f0
towelie <34988577+nabrus@users.noreply.github.com>
2021-04-03 11:10:37 -0400

    Complete block scope section

--- playground/scope.js
+++ playground/scope.js
@@ -1,26 +1,29 @@
-/* 
+/* ***********************************************
+
   SCOPE 
 
   Scope refers to where a variable or expression (i.e. function) can be accessed in a program.
   
   Scope is the idea in programming that some variables are accessible/inaccessible from other parts of the program.
-*/
 
-/* 
+*********************************************** */
+
+/* ***********************************************
+
   GLOBAL SCOPE - refers to the context within which variables/expressions are accessible to every part of the program.
 
   A browsers global scope is the `Window`
   **NOTE: declaring variables with `let` or `const` does not attach them to the window unlike `var`. But global variables are not good practice!
-*/
+
+*********************************************** */
 // Global variables are variables that exist within global scope.
 const first = 'Steve';
 
-/* 
+/* ***********************************************
 
-  Function Scope - variables that are only available inside the function in which they are declared.
+  FUNCTION SCOPE - variables that are only available inside the function in which they are declared.
 
-*/
-
+*********************************************** */
 // Global variable
 const age = 100;
 
@@ -37,4 +40,30 @@ function go() {
 }
 
 // Hair will not be available globally for example
-console.log(hair); // Uncaught ReferenceError: hair is not defined
+// console.log(hair); // Uncaught ReferenceError: hair is not defined
+
+/* ***********************************************
+
+  BLOCK SCOPE - variables, when declared with `let` or `const` that are only available within the corresponding block. A block refers to the curly brackets `{}` within `if`, `switch` conditions or `for` and `while` loops for example but not the `{}` of a function. Again, that would be function scope. 
+
+  function spaceCraft() {
+    if (true) {
+      var imperialShip = 'Tie Fighter';          // exists in function scope
+      const rebelShip = 'X-Wing';                // exists in block scope
+      let lightFreighter = 'Millennium Falcon';  // exists in block scope
+    }
+  }
+
+*********************************************** */
+function isCool(name) {
+  let cool; // exists in function scope
+  if (name === 'Steve') {
+    cool = true;
+  }
+  return cool;
+}
+/* eslint-disable */
+// Using `var` for this loop will alow the `i` variable to leak out to global scope. Using `let` here to declare `i` would make it block scoped.
+for(var i = 0; i < 10; i++) {
+  console.log(i);
+}
