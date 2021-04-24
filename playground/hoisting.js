f45328a8564e3cecfe1a1f662ddc7c25ad96572b
towelie <34988577+nabrus@users.noreply.github.com>
2021-04-24 10:23:29 -0400

    Add and complete hoisting exercise

--- playground/hoisting.js
+++ playground/hoisting.js
@@ -1,15 +1,21 @@
-/* esline-disable */
-let age;
-console.log(age);
-age = 10;
+/* eslint-disable */
 
-/* What does this file do? */
-sayHi();
+// Hoisting-when variable and function declarations are put into memory during the compile phase, enabling to invoke before declared. Virtually moving (hoisting) declarations to the top of your code. 
 
-/* How does this file do it? */
+// Function declarations
+sayHi(); // calling sayHI() before its declaration works due to hoisting
+
 function sayHi() {
-  console.log('hey!');
-  console.log(add(10, 2));
+  console.log('hey!'); // hey!
+  console.log(add(10, 2)); // 12
 }
 
-const add = (a, b) => a + b;
+// another example - still works even though it's called  in `sayHi` prior to declaration
+function add(a, b) {
+  return a + b;
+}
+
+// Variable declarations
+console.log(age); // undefined because only declarations are hoisted, no initialization has happened at this stage
+
+var age = 10;
