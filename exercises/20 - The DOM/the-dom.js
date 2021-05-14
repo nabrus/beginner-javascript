29123b1ad5083d070b1f5c9cc9471b85162c03c9
towelie <34988577+nabrus@users.noreply.github.com>
2021-05-13 22:00:56 -0400

    Complete selecting elements examples

--- exercises/20 - The DOM/the-dom.js
+++ exercises/20 - The DOM/the-dom.js
@@ -1,4 +1,11 @@
 const p = document.querySelector('p');
 const divs = document.querySelectorAll('div');
-console.log(p);
-console.log(divs);
+const itemAll = document.querySelectorAll('.item');
+const divItem = document.querySelectorAll('div.item');
+const imgs = document.querySelectorAll('.item img');
+
+console.log(p); // <p>Hi I'm an item</p>
+console.log(divs); // NodeList(3) [div.items, div.item, div.item]
+console.log(itemAll); // NodeList(3) [div.item, article.item, div.item]
+console.log(divItem); // NodeList(2) [div.item, div.item]
+console.log(imgs); // NodeList [img]
