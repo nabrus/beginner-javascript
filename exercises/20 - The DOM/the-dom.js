6ba37a194dbd4df608d4dbc4c3c7f88655ee198a
butters <34988577+nabrus@users.noreply.github.com>
2021-06-06 15:25:44 -0400

    Add two more element selections

--- exercises/20 - The DOM/the-dom.js
+++ exercises/20 - The DOM/the-dom.js
@@ -3,9 +3,13 @@ const divs = document.querySelectorAll('div');
 const itemAll = document.querySelectorAll('.item');
 const divItem = document.querySelectorAll('div.item');
 const imgs = document.querySelectorAll('.item img');
+const item2 = document.querySelector('.item2');
+const item2Image = item2.querySelector('img');
 
 console.log(p); // <p>Hi I'm an item</p>
 console.log(divs); // NodeList(3) [div.items, div.item, div.item]
 console.log(itemAll); // NodeList(3) [div.item, article.item, div.item]
 console.log(divItem); // NodeList(2) [div.item, div.item]
 console.log(imgs); // NodeList [img]
+console.log(item2); // <div class="item item2">...</div>
+console.log(item2Image); // <img src="https://picsum.photos/200">
