ff372121f0d05941e9a71e64e530041a881d1736
towelie <34988577+nabrus@users.noreply.github.com>
2021-06-08 22:23:12 -0400

    Add element property examples

--- exercises/20 - The DOM/the-dom.js
+++ exercises/20 - The DOM/the-dom.js
@@ -1,15 +1,25 @@
-const p = document.querySelector('p');
 const divs = document.querySelectorAll('div');
+const p = document.querySelector('p');
 const itemAll = document.querySelectorAll('.item');
 const divItem = document.querySelectorAll('div.item');
 const imgs = document.querySelectorAll('.item img');
 const item2 = document.querySelector('.item2');
 const item2Image = item2.querySelector('img');
 
-console.log(p); // <p>Hi I'm an item</p>
-console.log(divs); // NodeList(3) [div.items, div.item, div.item]
-console.log(itemAll); // NodeList(3) [div.item, article.item, div.item]
-console.log(divItem); // NodeList(2) [div.item, div.item]
-console.log(imgs); // NodeList [img]
-console.log(item2); // <div class="item item2">...</div>
-console.log(item2Image); // <img src="https://picsum.photos/200">
+// console.log(p); // <p>Hi I'm an item</p>
+// console.log(divs); // NodeList(3) [div.items, div.item, div.item]
+// console.log(itemAll); // NodeList(3) [div.item, article.item, div.item]
+// console.log(divItem); // NodeList(2) [div.item, div.item]
+// console.log(imgs); // NodeList [img]
+// console.log(item2); // <div class="item item2">...</div>
+// console.log(item2Image); // <img src="https://picsum.photos/200">
+
+const heading = document.querySelector('h2');
+// console.log(heading.textContent); // I am a heading
+// // Set the texContent property on the 'h2' element
+// heading.textContent = 'May the force be with you';
+console.log(heading.textContent); // May the force be with you
+console.log(heading.innerText);
+
+console.log(heading.innerHTML);
+console.log(heading.outerHTML);
