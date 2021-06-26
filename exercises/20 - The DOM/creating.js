84724c3604f19bd559ec191d428c8cd2eceb33c8
towelie <34988577+nabrus@users.noreply.github.com>
2021-06-26 15:31:46 -0400

    Add createElement example

--- exercises/20 - The DOM/creating.js
+++ exercises/20 - The DOM/creating.js
@@ -1,1 +1,8 @@
 // Creating HTML elements
+/*
+  `document.createElement()  method creates the HTML element specified by tagName
+*/
+const myParagraph = document.createElement('p');
+myParagraph.textContent = 'I am a P';
+myParagraph.classList.add('special');
+console.log(myParagraph);
