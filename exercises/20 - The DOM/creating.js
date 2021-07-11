c52738c99c09330f565d65226bf83f3c49054beb
butters <34988577+nabrus@users.noreply.github.com>
2021-07-11 09:11:39 -0400

    Create 3 new elements, adjust img size to 500

--- exercises/20 - The DOM/creating.js
+++ exercises/20 - The DOM/creating.js
@@ -2,7 +2,19 @@
 /*
   `document.createElement()  method creates the HTML element specified by tagName
 */
+
+// Create a 'p' element
 const myParagraph = document.createElement('p');
 myParagraph.textContent = 'I am a P';
 myParagraph.classList.add('special');
 console.log(myParagraph);
+
+// Create an 'img' element
+const myImage = document.createElement('img');
+myImage.src = 'https://picsum.photos/500';
+myImage.alt = 'Awesome photo!';
+
+// Create a 'div' element
+const myDiv = document.createElement('div');
+myDiv.classList.add('wrapper');
+console.log(myDiv);
