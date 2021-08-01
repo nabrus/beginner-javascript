21015ddedd39bf7fe5a082219190575eeab57de3
butters <34988577+nabrus@users.noreply.github.com>
2021-08-01 17:36:16 -0400

    Fix className wrapper to class

--- exercises/20 - The DOM/creating-with-strings.js
+++ exercises/20 - The DOM/creating-with-strings.js
@@ -4,7 +4,7 @@ const width = 500;
 const src = `https://picsum.photos/${width}`;
 const desc = `Random Pics`;
 const myHTML = `
-<div className="wrapper">
+<div class="wrapper">
   <h2>Cool ${desc}</h2> 
   <img src="${src}" alt="${desc}" />
 </div>
