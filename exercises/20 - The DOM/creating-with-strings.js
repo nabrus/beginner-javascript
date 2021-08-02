42470dbaa3937847a660a419fb0b2c9e08462a5c
butters <34988577+nabrus@users.noreply.github.com>
2021-08-01 22:59:53 -0400

    Complete creating HTML nodes example

--- exercises/20 - The DOM/creating-with-strings.js
+++ exercises/20 - The DOM/creating-with-strings.js
@@ -3,6 +3,8 @@ const item = document.querySelector('.item');
 const width = 500;
 const src = `https://picsum.photos/${width}`;
 const desc = `Random Pics`;
+
+// Note-the following is just a string not an element
 const myHTML = `
 <div class="wrapper">
   <h2>Cool ${desc}</h2> 
@@ -10,6 +12,18 @@ const myHTML = `
 </div>
 `;
 
+// This string can be dumped into to an element with innerHTML
 item.innerHTML = myHTML;
 
-console.log(item.innerHTML);
+// or if you need to access each node of the HTML
+
+// Turn a string into a DOM element/node
+const myFragment = document.createRange().createContextualFragment(myHTML);
+
+// Now the HTML is elements and can be selected
+
+console.log(myFragment.querySelector('img'));
+console.log(myFragment);
+
+// Then attach to the DOM
+document.body.appendChild(myFragment);
