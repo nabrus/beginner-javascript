82ed4f9fb54debbf314af78f1eb000ea14802b6a
towelie <34988577+nabrus@users.noreply.github.com>
2021-08-19 10:49:11 -0400

    Spelling correction in comment

--- exercises/20 - The DOM/creating.js
+++ exercises/20 - The DOM/creating.js
@@ -22,7 +22,7 @@ console.log(myDiv);
 /* 
 The Node.appendChild() method adds a node to the end of the list of children of a specified parent node.
 */
-// To avoid having to much re-paining, as would be with the order below,  change the order so it only re-paints once (twice for the image)
+// To avoid having to much re-painting, as would be with the order below,  change the order so it only re-paints once (twice for the image)
 // document.body.appendChild(myDiv);
 // myDiv.appendChild(myParagraph);
 // myDiv.appendChild(myImage);
