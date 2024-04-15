0ed3cf2a38477cf9ad677b002e394a8422bf4736
butters <34988577+nabrus@users.noreply.github.com>
2024-04-14 23:16:48 -0400

    Update to pic for addEventListener

--- exercises/20 - The DOM/the-dom.js
+++ exercises/20 - The DOM/the-dom.js
@@ -80,7 +80,7 @@ console.log(pic.naturalWidth); // getter, *note - naturalWidth is a getter only,
 pic.width = 200; // setter
 
 // naturalWidth shows zero on first console log because image not fully loaded yet. So the following can be used to get the correct output of 500...
-window.addEventListener('load', () => {
+pic.addEventListener('load', () => {
   console.log(pic.naturalWidth);
 });
 
