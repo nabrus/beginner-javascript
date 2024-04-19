165273915ed7c3f8f6d3e577fdd5305d9062319a
butters <34988577+nabrus@users.noreply.github.com>
2024-04-18 22:40:07 -0400

    Add examples of tagName to createElement definition

--- exercises/20 - The DOM/creating.js
+++ exercises/20 - The DOM/creating.js
@@ -1,6 +1,6 @@
 // Creating HTML elements
 /*
-  `document.createElement()` method creates the HTML element specified by tagName
+  `document.createElement()` method creates the HTML element specified by tagName (ex. `div`, `p`, `span`, `img`, etc)
 */
 
 // Create a 'p' element
