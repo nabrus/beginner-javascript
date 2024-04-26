e8bf36cf39c1a835aad70ed853a77f42775821f0
butters <34988577+nabrus@users.noreply.github.com>
2024-04-25 22:31:37 -0400

    Add definitions for methods used

--- exercises/20 - The DOM/creating-with-strings.js
+++ exercises/20 - The DOM/creating-with-strings.js
@@ -22,12 +22,33 @@ item.innerHTML = myHTML;
 
 // or if you need to access each node of the HTML
 
+/*
+
+In JavaScript, the Range interface represents a range of 
+elements in the DOM (Document Object Model). It provides a way 
+to select a portion of a document, such as a portion of text or 
+a range of nodes, and manipulate it programmatically. The Range 
+interface is commonly used in tasks such as text selection, 
+highlighting, and editing.
+
+Use the Document.createRange() method to create a range
+
+
+The createContextualFragment() method is a JavaScript method 
+available on the Range object. It allows you to create a new 
+DocumentFragment from a string of HTML or XML markup. This 
+method is particularly useful when you want to dynamically 
+generate DOM elements from a string of HTML and insert them 
+into the document.
+
+*/
+
 // Turn a string into a DOM element/node
 const myFragment = document.createRange().createContextualFragment(myHTML);
 
 // Now the HTML is elements and can be selected
-console.log(myFragment.querySelector('img'));
+// console.log(myFragment.querySelector('img'));
 console.log(myFragment);
 
 // Then attach to the DOM
-document.body.appendChild(myFragment);
+// document.body.appendChild(myFragment);
