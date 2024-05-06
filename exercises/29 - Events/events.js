dc7eea6c0caf532d0494f5917aada22d5fefdab3
butters <34988577+nabrus@users.noreply.github.com>
2024-05-06 00:06:52 -0400

    Start exercises

--- exercises/29 - Events/events.js
+++ exercises/29 - Events/events.js
@@ -1,53 +1,39 @@
+/*
+`addEventListener()` is a method in JavaScript that allows you to attach an
+event listener to an HTML element. This event listener listens for a specific
+event (such as a mouse click, a keypress, or a form submission) and executes a
+JavaScript function (or runs some JavaScript code) when that event occurs.
+
+syntax:
+element.addEventListener(event, function, options);
+
+  * element: The DOM element to which you want to attach the event listener.
+
+  * event: A string representing the name of the event you want to listen for (e.g., 'click', 'mouseover', 'keydown', etc.).
+
+  * function: The JavaScript function that will be executed when the specified event occurs. This function is often referred to as the "event handler" or "event callback".
+
+  * options (optional): An optional object that specifies additional options for the event listener, such as once, capture, or passive.
+
+Note: the function passed in can be referred to as a "callback function".
+A callback function is a function that is passed as an argument to another
+function and is executed later, usually in response to an event or when a
+certain condition is met.
+
+In the context of `addEventListener()`, the callback function represents the
+code that you want to execute when the specified event occurs. When the event
+occurs, the browser will call the callback function, allowing you to respond to
+the event appropriately.
+
+*/
+
 const butts = document.querySelector('.butts');
-const coolButton = document.querySelector('.cool');
+// Passing in an anonymous function
+// butts.addEventListener('click', () => alert('Downloading Virus'));
 
+// or passing in a named function
 function handleClick() {
-  console.log('🐛 IT GOT CLICKED!!!');
+  alert('!!Downloading Virus!! pwnd');
 }
 
-const hooray = () => console.log('HOORAY!');
-
-butts.addEventListener('click', function() {
-  console.log('Im an anon!');
-});
-coolButton.addEventListener('click', hooray);
-
-butts.removeEventListener('click', handleClick);
-
-// Listen on multiple items
-const buyButtons = document.querySelectorAll('button.buy');
-
-function handleBuyButtonClick(event) {
-  console.log('You clicked a button!');
-  const button = event.target;
-  // console.log(button.textContent);
-  // console.log(parseFloat(event.target.dataset.price));
-  console.log(event.target);
-  console.log(event.currentTarget);
-  console.log(event.target === event.currentTarget);
-  // Stop this event from bubbling up
-  // event.stopPropagation();
-}
-
-buyButtons.forEach(function(buyButton) {
-  buyButton.addEventListener('click', handleBuyButtonClick);
-});
-
-window.addEventListener(
-  'click',
-  function(event) {
-    console.log('YOU CLICKED THE WINDOW');
-    console.log(event.target);
-    console.log(event.type);
-    // event.stopPropagation();
-    console.log(event.bubbles);
-  },
-  { capture: true }
-);
-
-const photoEl = document.querySelector('.photo');
-
-photoEl.addEventListener('mouseenter', e => {
-  console.log(e.currentTarget);
-  console.log(this);
-});
+butts.addEventListener('click', handleClick);
