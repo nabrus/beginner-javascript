60f541fdc80387ec7ac3b0870374b5610a8d31bd
butters <34988577+nabrus@users.noreply.github.com>
2024-05-10 22:48:18 -0400

    Add event.target and event.currentTarget examples and def

--- exercises/29 - Events/events2.js
+++ exercises/29 - Events/events2.js
@@ -14,8 +14,30 @@ When an event occurs and triggers an event handler function (also known as a cal
 // Handler function
 function handleBuyButtonClick(event) {
   // name of arg doesn't matter, the first arg will return the event object
-  console.log(event);
+  // console.log(event); // PointerEvent {isTrusted: true, pointerId: 1, width: 1, height: 1, pressure: 0, …}
+  // console.log(parseInt(event.target.dataset.price));
+  // console.log(button.textContent);
+  const button = event.target;
+  console.log(event.target);
+  console.log(event.currentTarget);
+  console.log(event.target === event.currentTarget);
 }
+
+/*
+event.target:
+  event.target refers to the element that triggered the event. It represents the actual element on which the event occurred.
+
+  For example, if you click on a nested element inside a parent element, event.target will point to the nested element—the one directly clicked.
+
+  event.target always points to the element that originally dispatched the event, even if the event has propagated through multiple nested elements.
+
+event.currentTarget:
+  event.currentTarget refers to the element that the event listener is attached to. It represents the element that is currently handling the event.
+
+  For example, if you have a click event listener attached to a parent element, and you click on a child element inside that parent, event.currentTarget will point to the parent element—the one that the event listener is attached to.
+  
+  event.currentTarget can change as the event propagates through the DOM hierarchy, especially in the context of event delegation or bubbling.
+*/
 
 // Loop over each button and attach a listener
 buyButtons.forEach(function(buyButton) {
