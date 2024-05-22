6d2971fa7218c3d96ae96f0849f784ba4e512e59
butters <34988577+nabrus@users.noreply.github.com>
2024-05-21 23:15:26 -0400

    Add preventDefault method definition

---
+++ exercises/29 - Events/forms.js
@@ -0,0 +1,7 @@
+/*
+`preventDefault` is a method of the Event interface. It is used to cancel the default action that belongs to an event.
+Default behavior examples:
+  Clicking on a link (<a>) will navigate to the URL specified in the href attribute.
+  Submitting a form (<form>) will send the form data to the server and reload the page.
+  Pressing a key in a text input field will insert the corresponding character into the field.
+*/
