5f99934cd48fff896cbeb687bf6e0fefd239afa0
butters <34988577+nabrus@users.noreply.github.com>
2024-06-08 23:15:07 -0400

    Start setting up canvas to draw

--- exercises/33 - Etch-a-Sketch/etch-a-sketch.js
+++ exercises/33 - Etch-a-Sketch/etch-a-sketch.js
@@ -4,13 +4,28 @@ The Canvas API provides a means for drawing graphics via JavaScript and the HTML
 
 // Select elements on the page - canvas, shake button
 const canvas = document.querySelector('#etch-a-sketch');
+// `.getContext()` method gets that element's context—the thing onto which the drawing will be rendered.
 const ctx = canvas.getContext('2d');
 const shakeButt = document.querySelector('.shake');
 
 // Setup our canvas for drawing
+// Make a variable called width and height from the same properties on our canvas
+/* 
+const width = canvas.width;
+const height = canvas.width;
+This can be changed into a shorter form, which is called "destructuring"
+*/
+const { width, height } = canvas;
+
+console.log(width, height);
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
 ctx.lineWidth = 10;
+
+ctx.beginPath(); // start the drawing
+ctx.moveTo(200, 200); // location
+ctx.lineTo(200, 200); // location
+ctx.stroke(); // draw it
 
 // Write a draw function
 
