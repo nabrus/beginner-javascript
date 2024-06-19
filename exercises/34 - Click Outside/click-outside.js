ff1666738478ec3ea11f27892979b041dfb3d4c1
butters <34988577+nabrus@users.noreply.github.com>
2024-06-18 23:13:28 -0400

    Start handle click function

--- exercises/34 - Click Outside/click-outside.js
+++ exercises/34 - Click Outside/click-outside.js
@@ -1,1 +1,11 @@
-console.log('it works');
+const cardButtons = document.querySelectorAll('.card button');
+
+function handleCardButtonClick(e) {
+  const button = e.currentTarget;
+  const card = button.closest('.card');
+  console.log(card);
+}
+
+cardButtons.forEach(button =>
+  button.addEventListener('click', handleCardButtonClick)
+);
