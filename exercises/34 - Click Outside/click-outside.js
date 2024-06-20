6c8e2d4ebd79d74b3da91388409146fa5cfda7da
butters <34988577+nabrus@users.noreply.github.com>
2024-06-19 23:20:16 -0400

    Complete click outside modal function

--- exercises/34 - Click Outside/click-outside.js
+++ exercises/34 - Click Outside/click-outside.js
@@ -2,8 +2,8 @@ const cardButtons = document.querySelectorAll('.card button');
 const modalInner = document.querySelector('.modal-inner');
 const modalOuter = document.querySelector('.modal-outer');
 
-function handleCardButtonClick(e) {
-  const button = e.currentTarget;
+function handleCardButtonClick(event) {
+  const button = event.currentTarget;
   const card = button.closest('.card');
   // Grab the img src
   const imgSrc = card.querySelector('img').src;
@@ -21,3 +21,23 @@ function handleCardButtonClick(e) {
 cardButtons.forEach(button =>
   button.addEventListener('click', handleCardButtonClick)
 );
+
+// Close modal by removing the `open` class
+function closeModal() {
+  modalOuter.classList.remove('open');
+}
+
+// Close by clicking any area outside
+modalOuter.addEventListener('click', function(event) {
+  const isOutside = !event.target.closest('.modal-inner'); // boolean `not`
+  if (isOutside) {
+    closeModal();
+  }
+});
+
+// esc key to close modal also
+window.addEventListener('keydown', event => {
+  if (event.key === 'Escape') {
+    closeModal();
+  }
+});
