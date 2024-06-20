a7300a04c0024f8cdeb04a0273af48f615b6e947
butters <34988577+nabrus@users.noreply.github.com>
2024-06-19 22:34:24 -0400

    Complete handler function

--- exercises/34 - Click Outside/click-outside.js
+++ exercises/34 - Click Outside/click-outside.js
@@ -1,9 +1,21 @@
 const cardButtons = document.querySelectorAll('.card button');
+const modalInner = document.querySelector('.modal-inner');
+const modalOuter = document.querySelector('.modal-outer');
 
 function handleCardButtonClick(e) {
   const button = e.currentTarget;
   const card = button.closest('.card');
-  console.log(card);
+  // Grab the img src
+  const imgSrc = card.querySelector('img').src;
+  const desc = card.dataset.description;
+  const name = card.querySelector('h2').textContent;
+  // Populate modal with new info
+  modalInner.innerHTML = `
+  <img src="${imgSrc.replace('200', '600')}" alt="${name}" />
+  <p>${desc}</p>
+  `;
+  // Show the modal
+  modalOuter.classList.add('open');
 }
 
 cardButtons.forEach(button =>
