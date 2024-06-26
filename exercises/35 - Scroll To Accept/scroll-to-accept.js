23b40c8441133f924bc25f32bb6bdc7900b5fa55
butters <34988577+nabrus@users.noreply.github.com>
2024-06-25 23:20:05 -0400

    Start fist intersection observer example

--- exercises/35 - Scroll To Accept/scroll-to-accept.js
+++ exercises/35 - Scroll To Accept/scroll-to-accept.js
@@ -1,5 +1,14 @@
-const terms = document.querySelector('.terms-and-conditions');
+// Intersection Observer API
 
-terms.addEventListener('scroll', function(e) {
-  console.log(e);
-});
+// 1st example - when is the strong tag visible on page
+
+const watch = document.querySelector('.watch');
+
+function obCallback(payload) {
+  console.log(payload);
+}
+const ob = new IntersectionObserver(obCallback);
+
+ob.observe(watch);
+
+// const terms = document.querySelector('.terms-and-conditions');
